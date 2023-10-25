05b29add2b133e15bd4cd4228cd5704d6255a2a2
shivamgoyal26 <officialshivamgoyal@gmail.com>
2023-10-25 14:06:40 +0530

    Rooms and servers added

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,6 @@
+const userName = "shivam";
+const password = "shivam";
+
 const socket = io("http://localhost:9000");
 
 socket.on("connect", () => {
@@ -5,6 +8,23 @@ socket.on("connect", () => {
   socket.emit("clientConnect");
 });
 
-socket.on("welcome", (data) => {
-  console.log("data from server", data);
+// listen for nsList events from the server which gives us the namespaces
+socket.on("nsList", (nsData) => {
+  console.log(nsData);
+  // update HTML with each ns
+  const nameSpacesDiv = document.querySelector(".namespaces");
+  nameSpacesDiv.innerHTML = "";
+  nsData.forEach((ns) => {
+    nameSpacesDiv.innerHTML += `  <div class="namespace" ns="${ns.endpoint}"><img src="${ns.image}"></div>`;
+  });
+
+  Array.from(document.getElementsByClassName("namespace")).forEach(
+    (element) => {
+      element.addEventListener("click", (e) => {
+        joinNs(element, nsData);
+      });
+    }
+  );
+
+  joinNs(document.getElementsByClassName("namespace")[0], nsData);
 });
--- slack.js
+++ slack.js
@@ -3,11 +3,15 @@ const app = express();
 const socketio = require("socket.io");
 
 app.use(express.static(__dirname + "/public"));
+const namespaces = require("./data/namespaces");
 
 const expressServer = app.listen(9000);
 const io = socketio(expressServer);
 
 io.on("connection", (socket) => {
-  console.log(socket.id + "is connected!");
   socket.emit("welcome", "Welcome to the server");
+  socket.on("clientConnect", () => {
+    console.log(socket.id + "is connected!");
+  });
+  socket.emit("nsList", namespaces);
 });
